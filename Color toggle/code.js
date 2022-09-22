db631f27400df2bd1db91ca57e11a859c277979a
Karolis1997 <110409354+Karolis1997@users.noreply.github.com>
2022-09-22 14:28:02 +0200

    Add files via upload

---
+++ Color toggle/code.js
@@ -0,0 +1,33 @@
+    var burger = burgermenu;
+  
+  function Grey(){
+    document.body.style.background = "grey";
+    burger.click();
+    
+
+  }
+
+  function Blue(){
+    document.body.style.background = "blue";
+    burger.click();
+  }
+
+  function Red(){
+    document.body.style.background = "red";
+    burger.click();
+  }
+
+  function Green(){
+    document.body.style.background = "green";
+    burger.click();
+  }
+
+  function Yellow(){
+    document.body.style.background = "yellow";
+    burger.click();
+  }
+
+  function Start(){
+    document.body.style.background = "#232323";
+    burger.click();
+  }
